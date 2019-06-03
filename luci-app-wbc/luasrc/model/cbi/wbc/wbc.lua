b0f413c364ae2f6a45c7399d00e55c5e8bbe737c
libc0607 <libc0607@gmail.com>
2019-06-03 22:45:49 +0800

    Bug fixes

--- luci-app-wbc/luasrc/model/cbi/wbc/wbc.lua
+++ luci-app-wbc/luasrc/model/cbi/wbc/wbc.lua
@@ -177,11 +177,13 @@ o_video_keyframerate.default = 5
 o_video_keyframerate.placeholder = 5
 o_video_keyframerate.datatype = "range(2,10)"
 o_video_keyframerate:depends("mode", "tx")
+--[[
 -- wbc.video.extraparams: raspivid Extra Params
 o_video_extraparams = s_video:option(Value, "extraparams", translate("raspivid Extra Params"))
 o_video_extraparams.default = '-cd H264 -n -fl -ih -pf high -if both -ex sports -mm average -awb horizon'
 o_video_extraparams.placeholder = '-cd H264 -n -fl -ih -pf high -if both -ex sports -mm average -awb horizon'
 o_video_extraparams:depends("mode", "tx")
+]]
 -- wbc.video.save_enable: Video Save Enable
 o_video_save_enable = s_video:option(Flag, "save_enable", translate("Enable Video Save"))
 o_video_save_enable.rmempty = false
